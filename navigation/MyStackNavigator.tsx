16d7cfd3fed5dc187e30c01359edf8065df1f946
SJamal08 <sidikjam08@gmail.com>
2022-07-21 13:28:32 +0200

    add navigation

---
+++ navigation/MyStackNavigator.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from '../screens/Login';
+import Register from '../screens/Register';
+
+const Stack = createNativeStackNavigator();
+
+const MyStackNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MyStackNavigator;
